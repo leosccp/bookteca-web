4fd69360ed6a52c70fb37814e874bb638d32fc9f
Andrew Ribeiro <andrew.ribeiro@etec.sp.gov.br>
2019-08-07 22:29:16 -0300

    Criação de validação de formulário com JQuery Validation

--- livro.js
+++ livro.js
@@ -1,19 +1,54 @@
 window.onload = () => {
   let elementoBotao = document.querySelector('#cadastrar')
 
-  elementoBotao.addEventListener('click', (event) => {
-    event.preventDefault()
-    console.log('Clicou no botão')
-    let ladda = Ladda.create(elementoBotao)
-    ladda.start()
-    let progressoAtual = 0
-    let interval =
-    setInterval(() => {
-      ladda.setProgress(progressoAtual += 0.1)
-      if(progressoAtual >= 1) {
-        clearInterval(interval)
-        ladda.stop()
+  // elementoBotao.addEventListener('click', (event) => {
+  //   event.preventDefault()
+  //   let ladda = Ladda.create(elementoBotao)
+  //   ladda.start()
+  //   let progressoAtual = 0
+  //   let interval =
+  //   setInterval(() => {
+  //     ladda.setProgress(progressoAtual += 0.1)
+  //     if(progressoAtual >= 1) {
+  //       clearInterval(interval)
+  //       ladda.stop()
+  //     }
+  //   }, 1000)
+  // })
+
+  let formularioLivro = $('#formLivro')
+  formularioLivro.validate({
+    rules: {
+      titulo: {
+        required: true,
+        minlength: 3
+      },
+      subtitulo: {
+        required: false,
+        minlength: 5
+      },
+      descricao: {
+        required: true,
+        minlength: 10
       }
-    }, 1000)
+    },
+    messages: {
+      titulo: {
+        required: 'O título é obrigatório.',
+        minlength: 'O título é muito curto.'
+      },
+      subtitulo: {
+        minlength: 'O subtitulo é muito curto.'
+      },
+      descricao: {
+        required: 'A descrição é obrigatória.',
+        minlength: 'A descrição deve ter mais do que 10 caracteres.'
+      }
+    },
+    errorPlacement: function(error, element) {
+      element.parent().parent().find('.error').append(error).addClass('animated shake')
+    },
+    errorClass: 'cor-do-erro'
   })
+
 }
