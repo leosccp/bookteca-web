5fd47b70f378888cb14fda816cca23c21e9153b8
LEONARDO RODRIGUES LEOPOLDINO <495950026@alunos.etec.itaqua>
2019-10-16 22:24:57 -0300

    Criação do laço for para exibição de todos os livros.

--- index.js
+++ index.js
@@ -2,37 +2,54 @@ $(function(){
   function carregarLivros() {
     fetch("http://localhost:8080/bookteca-api/src/lerlivros.php").then(function(resposta){
       resposta.json().then(function(json){
+
         let listagem = $("#listagem")
-        let coluna4 = $("<div></div>")
-        coluna4.addClass("col-4")
-        let card = $("<div></div>")
-        card.addClass("custom-card")
-        coluna4.append(card)
-        let cardHeader = $("<header></header>")
-        cardHeader.addClass("custom-card-header")
-        card.append(cardHeader)
-        let cardHeaderTitleContainer = $("<div></div>")
-        cardHeaderTitleContainer.addClass("custom-card-header-container")
-        let cardHeaderTitle = $("<strong></strong>")
-        cardHeaderTitle.append(json[0].titulo)
-        let cardHeaderSubtitle = $("<h6></h6>")
-        cardHeaderSubtitle.addClass("custom-card-header-subtitle")
-        cardHeaderSubtitle.append(json[0].subtitulo)
-        cardHeaderTitleContainer.append(cardHeaderTitle)
-        cardHeaderTitleContainer.append(cardHeaderSubtitle)
-        cardHeader.append(cardHeaderTitleContainer)
-        let cardHeaderIcon = $("<span></span>")
-        cardHeaderIcon.addClass("custom-card-header-icon")
-        cardHeader.prepend(cardHeaderIcon)
-        let cardImg = $("<div></div>")
-        cardImg.addClass("custom-card-img")
-        cardImg.css("background-image", 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcyrVELmlX_3f08FLeUF6jxLVdVTqLgMa2UTZGW_AJPEBWFjFH")')
-        card.append(cardImg)
-        listagem.append(coluna4)
+        for(let i = 0; i < json.length; i++){
+          let cardAtual = criarCard(json[i].titulo, json[i].subtitulo, json[i].descricao, "https://picsum.photos/200/300")
+          listagem.append(cardAtual)
+        }
+
       })
     })
   }
 
+  carregarLivros();
 
-  carregarLivros()
+  function criarCard(titulo, subtitulo, descricao, imagem){
+    let coluna4 = $("<div></div>")
+    coluna4.addClass("col-4")
+
+    let card = $("<div></div>")
+    card.addClass("custom-card")
+    coluna4.append(card)
+
+    let cardheader = $("<header></header>")
+    cardheader.addClass("custom-card-header")
+    card.append(cardheader)
+
+    let cardHeaderTitleContainer = $("<div></div>")
+    cardHeaderTitleContainer.addClass("custom-card-title-container")
+
+    let cardHeaderTitle = $("<strong></strong>")
+    cardHeaderTitle.append(titulo)
+
+    let cardHeaderSubtitle = $("<h6></h6>")
+    cardHeaderSubtitle.addClass("custom-card-header-subtitle")
+    cardHeaderSubtitle.append(subtitulo)
+
+    cardHeaderTitleContainer.append(cardHeaderTitle)
+    cardHeaderTitleContainer.append(cardHeaderSubtitle)
+    cardheader.append(cardHeaderTitleContainer)
+
+    let cardHeaderIcon = $("<span></span>")
+    cardHeaderIcon.addClass("custom-card-header-icon")
+    cardheader.prepend(cardHeaderIcon)
+
+    let cardImg = $("<div></div>")
+    cardImg.addClass("custom-card-img")
+    cardImg.css("background-image",'url("' + imagem + '")')
+    card.append(cardImg)
+
+    return coluna4
+  }
 })
